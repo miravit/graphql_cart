99bb4c1b0dd89caadf5bfa2425838e9bcb6cdb0a
MAC <miravitsas1@gmail.com>
2023-02-01 11:13:33 +0100

    kommentarer på addProductToCart

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -107,50 +107,55 @@ exports.resolvers = {
       return newProduct;
     },
     addProductToCart: async (_, args) => {
-      const { cartId, productId } = args;
+      const { cartId, productId } = args; //hämta cartid och product id. Så att man kan skriva in vilka man väljer i apollo.
 
-      const filePath = path.join(cartDirectory, `${cartId}.json`);
-      const shoppingCartExists = await fileExists(filePath);
+      const filePath = path.join(cartDirectory, `${cartId}.json`); //hitta carten med det id som skrivits in i args
+      const cartExist = await fileExists(filePath); //om filen finns läggs den i cartExist.
 
-      if (!shoppingCartExists) {
-        return new GraphQLError("This cart does not exist!");
+      if (!cartExist) {
+        return new GraphQLError("This cart does not exist!"); //om carten med det it inte hittas. returnera felmeddelande
       }
 
-      const productFilepath = path.join(productDirectory, `${productId}.json`);
-      const productExists = await fileExists(productFilepath);
+      const productFilepath = path.join(productDirectory, `${productId}.json`); // hitta productsfilen med id som skrevs in i args.
+      const productExist = await fileExists(productFilepath); //om filen finns lagras den i variabeln productexist.
 
-      if (!productExists) {
-        return new GraphQLError("This product doesn't exist!");
+      if (!productExist) {
+        return new GraphQLError("This product doesn't exist!"); //om det inte finns en fil med det id. Skicka felmeddelande
       }
 
-      const cartContents = await fsPromises.readFile(filePath, {
+      const cartFile = await fsPromises.readFile(filePath, {
+        //läser cartfilen inkl alla products den innehåller. filepath är pathen och encoding gör att vi kan läsa den tydligare
+        //products-responsen
         encoding: "utf-8",
       });
-      console.log(cartContents);
+      //console.log(cartFile);
 
-      let shoppingCart = JSON.parse(cartContents);
+      let shoppingCart = JSON.parse(cartFile); // gör om våran cartfile till json-objekt och lägger till i shoppingcartvariabeln. shoppingCart är alltså våran cart som innehåller alla products.
+      //console.log("kolla här:", shoppingCart);
 
-      const fileContents = await fsPromises.readFile(productFilepath, {
+      const newProduct = await fsPromises.readFile(productFilepath, {
+        //läser productfilen som väljs ut genom productid.
         encoding: "utf-8",
       });
+      //console.log(newProduct); nya produkten som med hjälp av producId läggs till i befintlig cart.
+      const productToCart = JSON.parse(newProduct); // gör om nya produkten som skapats från json-data till javascriptobjekt. nu innehåller alltså variabeln productToCart den nya produkten i javascriptformat eftersom att vi ska pusha in den i våran cart.
+      //vi gör om till javascriptobjekt eftersom att det inte går att arbeta med JSON i javascript.
 
-      const productToCart = JSON.parse(fileContents);
+      const products = shoppingCart.products; //alla produkter som finns i en specifik shoppingcart. Visar endast produkterna. loggar man shoppingcart ser man hela varukorgen med dess id och totalAmount, samt alla prductsobjekt inuti.
 
-      const products = shoppingCart.products;
-      shoppingCart.products.push(productToCart);
+      shoppingCart.products.push(productToCart); //pushar nya produkten vi valt genom productId och pushar in den i shoppingcart.products.Detta för vi vill att den ska läggas i products inuti carts och inte direkt i carts.
 
-      let totalamount = 0;
+      let totalamount = 0; //deklarerar totalsumman
       for (let i = 0; i < shoppingCart.products.length; i++) {
-        totalamount += shoppingCart.products[i].price;
+        //loopar igenom alla products som finns i varukorgen och skriver ut varje pris.
+        totalamount += shoppingCart.products[i].price; //totalamount plussas på med priset för varje varv den körs i loopen
       }
 
-      const updatedCart = { cartId, products, totalamount };
+      const updatedCart = { cartId, products, totalamount }; // hämtar nya cartId, products och totalamount och
 
-      await fsPromises.writeFile(filePath, JSON.stringify(updatedCart));
+      await fsPromises.writeFile(filePath, JSON.stringify(updatedCart)); //skriver över gamla filen efersom den har samma cartId. gör även om från javascript till JSON-data. Den uppdaterar alltså gamla varukorgen
 
-      // console.log(shoppingcart.products);
-
-      return updatedCart;
+      return updatedCart; //returnerar den uppdaterade varukorgen så vi kan göra om allt!
     },
   },
 };
